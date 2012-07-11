de336f69d8938ffdd29f06d555f76d49be1023ad
Mihael Konjevic <konjevic@gmail.com>
2012-07-11 21:42:03 +0200

    Work on styling

--- public/kekomi/admin/nav/nav.js
+++ public/kekomi/admin/nav/nav.js
@@ -1,9 +1,16 @@
-steal('admin/mvc.js', 'admin/vendor/inflection', function(){
+steal('admin/mvc.js', 'admin/vendor/inflection', 'steal/less').then('./nav.less', function(){
 	can.Control('Admin.Nav', {}, {
 		init : function(){
 			this.element.html(this.view('init', {
 				contentTypes: Admin.Models.ContentType.all()
 			}))
+		},
+		".dropdown-toggle click" : function(el, ev){
+			ev.stopPropagation();
+			el.siblings('.dropdown-menu').toggle();
+		},
+		"{document} click" : function(el, ev){
+			this.element.find('.dropdown-menu').hide();
 		},
 		"{can.route} type change" : function(){
 			var control = "admin_" + can.route.attr('type');
