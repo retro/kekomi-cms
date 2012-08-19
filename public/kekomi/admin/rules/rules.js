02d2a2fd0319200781ac0277c58cc9a5376b1d3f
Mihael Konjevic <konjevic@gmail.com>
2012-08-19 15:01:45 +0200

    More work on the UI for slots

--- public/kekomi/admin/rules/rules.js
+++ public/kekomi/admin/rules/rules.js
@@ -11,13 +11,15 @@ steal(
 		},
 		render : function(contentTypes){
 			this.contentTypes = contentTypes;
-			this.element.html(this.view('init'))
+			this.element.html(this.view('init'));
+			this.recalculateRules();
 		},
 		update : function(){
 			this._super.apply(this, arguments);
 			this.recalculateRules();
 		},
 		recalculateRules : function(){
+			delete this.sharedFieldsList;
 			var options = this.element.find('select.rule option');
 			var types   = this.presentFieldTypes();
 			if(this.options.selectedContentTypes.length === 0){
@@ -29,49 +31,55 @@ steal(
 				var fieldType = $option.data('field-type');
 				if(typeof fieldType !== 'undefined' && types.indexOf(fieldType) === -1){
 					$option.prop('disabled', true);
+				} else {
+					$option.prop('disabled', false);
 				}
-			})
+			});
 		},
 		sharedFields : function(){
-			var fields            = {};
-			var contentTypes      = this.contentTypes.get(this.options.selectedContentTypes);
-			var contentTypeFields = [];
-			var wasFound          = false;
-			for(var i = 0; i < contentTypes.length; i++){
-				contentTypeFields = contentTypes[i].attr('fields').attr();
-				if($.isEmptyObject(fields)){
-					for(var j = 0; j < contentTypeFields.length; j++){
-						if(['string', 'date'].indexOf(contentTypeFields[j].type) !== -1){
-							fields[contentTypeFields[j].name] = contentTypeFields[j].type;
+			if(typeof this.sharedFieldsList === "undefined"){
+				var fields            = {};
+				var contentTypes      = this.contentTypes.get(this.options.selectedContentTypes);
+				var contentTypeFields = [];
+				var wasFound          = false;
+				for(var i = 0; i < contentTypes.length; i++){
+					contentTypeFields = contentTypes[i].attr('fields').attr();
+					if($.isEmptyObject(fields)){
+						for(var j = 0; j < contentTypeFields.length; j++){
+							if(['string', 'date'].indexOf(contentTypeFields[j].type) !== -1){
+								fields[contentTypeFields[j].name] = contentTypeFields[j].type;
+							}
 						}
-					}
-				} else {
-					for(var field in fields){
-						wasFound = false;
-						if(fields.hasOwnProperty(field)){
-							for(var j = 0; j < contentTypeFields.length; j++){
-								if(field === contentTypeFields[j].name){
-									wasFound = true;
-									if(fields[field] !== contentTypeFields[j].type){
-										delete fields[field];
+					} else {
+						for(var field in fields){
+							wasFound = false;
+							if(fields.hasOwnProperty(field)){
+								for(var j = 0; j < contentTypeFields.length; j++){
+									if(field === contentTypeFields[j].name){
+										wasFound = true;
+										if(fields[field] !== contentTypeFields[j].type){
+											delete fields[field];
+										}
 									}
 								}
 							}
-						}
-						if(!wasFound){
-							delete fields[field];
+							if(!wasFound){
+								delete fields[field];
+							}
 						}
 					}
 				}
+				if(!$.isEmptyObject(fields)){
+					$.extend(fields, {
+						published_at : 'date',
+						tags : 'tags'
+					});
+				}
+				this.sharedFieldsList = fields;
+				return fields;
+			} else {
+				return this.sharedFieldsList;
 			}
-			if(!$.isEmptyObject(fields)){
-				$.extend(fields, {
-					published_at : 'date',
-					tags : 'tags'
-				});
-			}
-			console.log(fields)
-			return fields;
 		},
 		presentFieldTypes : function(){
 			var fields = this.sharedFields();
@@ -82,6 +90,17 @@ steal(
 				}
 			}
 			return types.unique();
+		},
+		".add-rule click" : function(el, ev){
+			var rule = this.element.find('.rule').val();
+			if(rule !== ""){
+				this.element.find('.rules').append(this.view('rules/' + rule, {
+					fields : this.sharedFields()
+				}))
+			}
+		},
+		".remove-rule click" : function(el, ev){
+			el.closest('.rule-wrap').remove();
 		}
 	})
 })
--- public/kekomi/admin/slots/form/form.js
+++ public/kekomi/admin/slots/form/form.js
@@ -6,15 +6,20 @@ steal(
 
 	can.Control('Admin.Slots.Form', {}, {
 		init : function(){
+			Admin.Models.Page.findAll({}, this.proxy('render'))
+		},
+		render : function(pages){
 			this.element.html(this.view('init', {
-				pages : Admin.Models.Page.findAll({})
+				pages : pages
 			})).addClass('paper-form');
+			this.element.find('input[type=checkbox]').prop('checked', true).filter(':first').trigger('change')
 		},
 		'.source-section change' : function(){
 			var values = this.element.find('.source-section:checked').map(function(){
 				return this.value;
 			}).get().unique();
 			this.element.find('.admin_rules').admin_rules({selectedContentTypes: values});
+			this.element.find('.admin-rules-wrap').toggle(values.length !== 0);
 		}
 	})
 })
